3a39cdeb5415a9e673f0a6a351a117831680fb1b
golden-expiriensu <gundari_dan@bk.ru>
2022-12-12 22:17:43 +0700

    made aptos service belong to eth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
-import { AptosModule } from "./aptos/aptos.module";
 import { DBAccessModule } from "./db-access/db-access.module";
 import { EthModule } from "./eth/eth.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), DBAccessModule, EthModule, AptosModule],
+  imports: [ConfigModule.forRoot(), DBAccessModule, EthModule],
 })
 export class AppModule {}
--- backend/src/aptos/aptos.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AptosService } from "./aptos.service";
-
-@Module({
-  providers: [AptosService],
-})
-export class AptosModule {}
