32d1606f7c36d7f19adc597a8eaecaffa2a9db19
golden-expiriensu <gundari_dan@bk.ru>
2022-12-13 18:11:53 +0700

    added ssl

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const httpsOptions = {
+    key: readFileSync(join(process.env.CERTS_PATH, 'key.pem')),
+    cert: readFileSync(join(process.env.CERTS_PATH, 'cert.pem')),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+  await app.listen(3000);
 }
+
 bootstrap()
